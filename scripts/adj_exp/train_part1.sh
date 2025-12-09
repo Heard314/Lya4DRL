8a94b5c23bb6ef231511051ee1f8950278280f33
buaa <2567995679@qq.com>
2025-12-09 20:04:45 +0800

    修改实验脚本

--- scripts/adj_exp/train_part1.sh
+++ scripts/adj_exp/train_part1.sh
@@ -3,13 +3,13 @@
 echo "===== Running exp1.sh ====="
 ./exp1.sh
 
-echo "===== Running exp2.sh ====="
-./exp2.sh
-
 echo "===== Running exp3.sh ====="
 ./exp3.sh
 
-echo "===== Running exp4.sh ====="
-./exp4.sh
+echo "===== Running exp5.sh ====="
+./exp5.sh
+
+echo "===== Running exp7.sh ====="
+./exp7.sh
 
 echo "===== All experiments finished! ====="
--- scripts/adj_exp/train_part2.sh
+++ scripts/adj_exp/train_part2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "===== Running exp5.sh ====="
-./exp5.sh
+echo "===== Running exp2.sh ====="
+./exp2.sh
+
+echo "===== Running exp4.sh ====="
+./exp4.sh
 
 echo "===== Running exp6.sh ====="
 ./exp6.sh
-
-echo "===== Running exp7.sh ====="
-./exp7.sh
 
 echo "===== Running exp8.sh ====="
 ./exp8.sh
