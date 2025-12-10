8a54e0f8e80597faf280e92d167a186a0b739f07
buaa <2567995679@qq.com>
2025-12-11 00:00:25 +0800

    修改实验脚本

--- scripts/adj_exp/exp4.sh
+++ scripts/adj_exp/exp4.sh
@@ -1,7 +1,7 @@
 python ../../main.py --train_mode mappo --run_desc "no_queue_exp4" \
     --edge_queue_reward_weight -700 --device_queue_reward_weight -300 \
-    --device_act_queue_reward_max_bound 600 --device_act_queue_reward_min_bound -800 \
-    --device_vir_queue_reward_max_bound 1600 --device_vir_queue_reward_min_bound -2000 \
+    --device_act_queue_reward_max_bound 300 --device_act_queue_reward_min_bound -450 \
+    --device_vir_queue_reward_max_bound 1200 --device_vir_queue_reward_min_bound -1800 \
     --device_act_queue_growth_rate 1 --device_vir_queue_growth_rate 0.1 \
     --edge_act_queue_growth_rate 1 --edge_vir_queue_growth_rate 0.1 \
     --timeout_reward_penalty -4000 --target_reward_penalty -2000
