283a3b88c20528fe49ccbd4ebb1757ad13d0ea73
buaa <2567995679@qq.com>
2026-01-24 18:42:43 +0800

    测试脚本修改

--- scripts/eval/ppo_adj_exp/exp10.sh
+++ scripts/eval/ppo_adj_exp/exp10.sh
@@ -1,5 +1,1 @@
-python ../../../main.py --evaluate --eval_mode mappo --run_desc "no_queue_exp10"\
-    --load_weights --resume_episode 29000 --weights_dir "weight/train/mappo_s_7878_t_2026-01-18-11-56-38-741022_d_no_queue_exp10/" \
-    --device_act_queue_growth_rate 1 --device_vir_queue_growth_rate 0.1 \
-    --edge_act_queue_growth_rate 1 --edge_vir_queue_growth_rate 0.1 \
-    --timeout_reward_penalty -4000 --target_reward_penalty -1000
+python ../../../main.py --evaluate --eval_mode mappo --run_desc "no_queue_exp10"
