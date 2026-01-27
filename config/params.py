d28d38fff46577bd1664626716c15ec8c45f7b40
buaa <2567995679@qq.com>
2026-01-27 23:56:29 +0800

    修改任务参数

--- config/params.py
+++ config/params.py
@@ -193,7 +193,7 @@ def get_general_params():
         type=list,
         # default=[[0.9, 1.2], [0.9, 1.0], [0.5, 2.0],
         #         [1.4, 1.5], [0.6, 1.2]],
-        default=[[1.4, 1.6], [0.6, 0.8], [2.6, 2.8]],
+        default=[[1.4, 1.6], [0.6, 0.8], [2.8, 3.0]],
         help="the data-size intervals of tasks (Mbits)"
     )
 
@@ -201,7 +201,7 @@ def get_general_params():
         "--comp_dens_inls",
         type=list,
         # default=[[0.3, 0.4], [0.6, 0.8], [0.15, 0.2]],
-        default=[[2.3, 2.5], [5.0, 5.2], [1.0, 1.2]],
+        default=[[2.0, 2.2], [4.2, 4.4], [1.0, 1.2]],
         help="the computation-density intervals of tasks (GFLOPs/Mbits)"
     )
     parser.add_argument("--comp_dly_thre", type = list, 
@@ -287,7 +287,7 @@ def get_general_params():
                         help = "The bound multi factor for edge queue reward")
 
     # navie reward
-    parser.add_argument("--base_reward_penalty", type = float, default = 10000,
+    parser.add_argument("--base_reward_penalty", type = float, default = 0,
                         help = "the base reward penalty for each task")
 
     parser.add_argument("--timeout_reward_penalty", type = float, default = -4000,
@@ -467,10 +467,10 @@ maddpg_time_slots = 3000
 maddpg_train_freq = 4
 maddpg_update_freq = 8
 maddpg_lr_decay_freq = 100
-maddpg_p_lr = 1e-5
-maddpg_v_lr = 1e-5
-maddpg_p_min_lr = 1e-7
-maddpg_v_min_lr = 1e-7
+maddpg_p_lr = 5e-5
+maddpg_v_lr = 1e-4
+maddpg_p_min_lr = 1e-6
+maddpg_v_min_lr = 1e-6
 def get_maddpg_params():
     parser = argparse.ArgumentParser(description = "maddpg params", add_help=False, allow_abbrev=False)
     
