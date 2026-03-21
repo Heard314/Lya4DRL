5a23a3cf74b236b51b52857d9ee8034cbf4c9115
buaa <2567995679@qq.com>
2026-03-21 17:16:45 +0800

    修改泛化实验脚本

--- scripts/train/env_gen_exp/exp1.sh
+++ scripts/train/env_gen_exp/exp1.sh
@@ -1,3 +1,3 @@
-python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e40" --enable_virtual_queue_reward --edge_comp_freq 47.5
-python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e55" --enable_virtual_queue_reward --edge_comp_freq 52.5
-python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e60" --enable_virtual_queue_reward --edge_comp_freq 55.0
+python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e47.5" --enable_virtual_queue_reward --edge_comp_freq 47.5
+python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e52.5" --enable_virtual_queue_reward --edge_comp_freq 52.5
+python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e55" --enable_virtual_queue_reward --edge_comp_freq 55.0
