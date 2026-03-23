96a8a3d296d6a5ca4601d744d58de24d96c3b24c
buaa <2567995679@qq.com>
2026-03-23 20:09:39 +0800

    补充泛化实验脚本

--- scripts/train/env_gen_exp/exp1.sh
+++ scripts/train/env_gen_exp/exp1.sh
@@ -1,3 +1,11 @@
 python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e47.5" --enable_virtual_queue_reward --edge_comp_freq 47.5
 python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e52.5" --enable_virtual_queue_reward --edge_comp_freq 52.5
 python ../../../main.py --train_mode maddpg --run_desc "my_exp1_e55" --enable_virtual_queue_reward --edge_comp_freq 55.0
+
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_e47.5" --enable_actual_queue_reward --edge_comp_freq 47.5
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_e52.5" --enable_actual_queue_reward --edge_comp_freq 52.5
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_e55" --enable_actual_queue_reward --edge_comp_freq 55.0
+
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_e47.5" --edge_comp_freq 47.5
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_e52.5" --edge_comp_freq 52.5
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_e55" --edge_comp_freq 55.0
--- scripts/train/env_gen_exp/exp2.sh
+++ scripts/train/env_gen_exp/exp2.sh
@@ -1,3 +1,11 @@
 python ../../../main.py --train_mode maddpg --run_desc "my_exp2_l2.25" --enable_virtual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 2.25
 python ../../../main.py --train_mode maddpg --run_desc "my_exp2_l2.75" --enable_virtual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 2.75
 python ../../../main.py --train_mode maddpg --run_desc "my_exp2_l3.0" --enable_virtual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 3.0
+
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_l2.25" --enable_actual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 2.25
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_l2.75" --enable_actual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 2.75
+python ../../../main.py --train_mode maddpg --run_desc "rt_exp_l3.0" --enable_actual_queue_reward --enable_device_comp_freq_chg --device_comp_freq 3.0
+
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_l2.25" --enable_device_comp_freq_chg --device_comp_freq 2.25
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_l2.75" --enable_device_comp_freq_chg --device_comp_freq 2.75
+python ../../../main.py --train_mode maddpg --run_desc "no_queue_l3.0" --enable_device_comp_freq_chg --device_comp_freq 3.0
