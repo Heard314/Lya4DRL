d282ebe462df8dfc92a8cad18be7f3f3a3f44b3b
local <2567995679@qq.com>
2026-06-15 13:35:49 +0800

    fix: 添加消融实验测试脚本

---
+++ scripts/train/env_ablation/device_freq_2.75.py
@@ -0,0 +1,32 @@
+"""Device computation frequency ablation: 2.75 Gcycles/s"""
+import sys, os
+os.chdir("D:/Desktop/work/yanjiushengbishe/mypaper/Lya4DRL")
+sys.path.insert(0, ".")
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "dev_freq_2.75",
+    "--enable_virtual_queue_reward",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-2000",
+]
+
+gen_params = get_general_params()
+gen_params.device_comp_freqs = [2.75, 2.75, 2.75]
+
+ctr = Controller(gen_params)
+ctr.train()
---
+++ scripts/train/env_ablation/edge_freq_52.5.py
@@ -0,0 +1,32 @@
+"""Edge server computation frequency ablation: 52.5 Gcycles/s"""
+import sys, os
+os.chdir("D:/Desktop/work/yanjiushengbishe/mypaper/Lya4DRL")
+sys.path.insert(0, ".")
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "edge_freq_52.5",
+    "--enable_virtual_queue_reward",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-2000",
+]
+
+gen_params = get_general_params()
+gen_params.edge_comp_freq = 52.5
+
+ctr = Controller(gen_params)
+ctr.train()
