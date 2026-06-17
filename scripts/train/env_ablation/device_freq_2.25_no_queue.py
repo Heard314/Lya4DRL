ce2604fd2a703dcc642641145a7f15d997aabb74
local <2567995679@qq.com>
2026-06-17 09:24:58 +0800

    feat: 添加消融实验脚本

---
+++ scripts/train/env_ablation/device_freq_2.25_no_queue.py
@@ -0,0 +1,33 @@
+"""Device computation frequency ablation: 2.25 Gcycles/s (no queue reward)"""
+import sys, os
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJ_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(_SCRIPT_DIR)))
+os.chdir(_PROJ_ROOT)
+sys.path.insert(0, _PROJ_ROOT)
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "dev_freq_2.25_no_queue",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-80",
+]
+
+gen_params = get_general_params()
+gen_params.device_comp_freqs = [2.25, 2.25, 2.25]
+
+ctr = Controller(gen_params)
+ctr.train()
---
+++ scripts/train/task_ablation/task_comp_plus1_act_queue.py
@@ -0,0 +1,35 @@
+"""Task computation ablation: +1 Gcycle average increase (actual queue reward only)"""
+import sys, os
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJ_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(_SCRIPT_DIR)))
+os.chdir(_PROJ_ROOT)
+sys.path.insert(0, _PROJ_ROOT)
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "task_comp_plus1_act_queue",
+    "--enable_actual_queue_reward",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-80",
+]
+
+gen_params = get_general_params()
+gen_params.data_size_inls = [[1.7, 2.1], [1.3, 1.6], [3.2, 4.2]]
+gen_params.comp_dens_inls = [[2.7, 3.2], [5.5, 6.5], [2.0, 2.4]]
+
+ctr = Controller(gen_params)
+ctr.train()
