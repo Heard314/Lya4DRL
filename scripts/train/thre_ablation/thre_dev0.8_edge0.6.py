2879d835eece840c5f56b3b796c193c508c76a9d
local <2567995679@qq.com>
2026-06-18 08:59:17 +0800

    fix: 修改脚本

---
+++ scripts/train/thre_ablation/thre_dev0.8_edge0.6.py
@@ -0,0 +1,35 @@
+"""Threshold ablation: device_dly_adj_fac=0.8, edge_dly_adj_fac=0.6"""
+import sys, os
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJ_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(_SCRIPT_DIR)))
+os.chdir(_PROJ_ROOT)
+sys.path.insert(0, _PROJ_ROOT)
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "thre_dev0.8_edge0.6",
+    "--enable_virtual_queue_reward",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-80",
+]
+
+gen_params = get_general_params()
+gen_params.device_dly_adj_fac = [0.8, 0.8, 0.8]
+gen_params.edge_dly_adj_fac = [0.6, 0.6, 0.6]
+
+ctr = Controller(gen_params)
+ctr.train()
