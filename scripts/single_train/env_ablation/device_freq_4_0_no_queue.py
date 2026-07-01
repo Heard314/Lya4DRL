dbc49073487015501b91aa6d36f47f9d480e06fe
local <2567995679@qq.com>
2026-07-02 00:00:51 +0800

    feat: 添加单服务器高设备计算频率的脚本

---
+++ scripts/single_train/env_ablation/device_freq_4_0_no_queue.py
@@ -0,0 +1,52 @@
+"""device_freq=4.0"""
+import sys, os
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+_PROJ_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(_SCRIPT_DIR)))
+os.chdir(_PROJ_ROOT)
+sys.path.insert(0, _PROJ_ROOT)
+
+from config.params import get_general_params
+from controller import Controller
+
+sys.argv = [
+    "main",
+    "--train_mode", "maddpg",
+    "--run_desc", "single_1s10d_device_freq_4_0_no_queue",
+    "--device_vir_queue_reward_weight", "-700",
+    "--device_act_queue_reward_weight", "-300",
+    "--device_act_queue_reward_max_bound", "200",
+    "--device_act_queue_reward_min_bound", "-300",
+    "--device_vir_queue_reward_max_bound", "800",
+    "--device_vir_queue_reward_min_bound", "-1200",
+    "--device_act_queue_growth_rate", "1",
+    "--device_vir_queue_growth_rate", "0.1",
+    "--edge_act_queue_growth_rate", "1",
+    "--edge_vir_queue_growth_rate", "0.1",
+    "--timeout_reward_penalty", "-4000",
+    "--target_reward_penalty", "-80",
+    # single server
+    "--edge_server_num", "1",
+    # network dims (S=1, device_num=10)
+    "--device_obs_dim", "6",
+    "--edge_queue_obs_dim", "2",
+    "--action_dim", "4",
+    "--value_input_obs_dim", "80",
+    "--value_input_act_dim", "40",
+    "--value_input_dims", "120",
+    "--policy_input_dim", "8",
+]
+
+gen_params = get_general_params()
+gen_params.device_types = [0]*2 + [1]*4 + [2]*4
+gen_params.device_in_types = [
+    list(range(0, 2)),
+    list(range(2, 6)),
+    list(range(6, 10)),
+]
+gen_params.device_num_per_type = [2, 4, 4]
+gen_params.data_size_inls = [[0.0, 4.0], [0.0, 2.0], [0.0, 1.0]]
+
+gen_params.device_comp_freqs = [4.0, 4.0, 4.0]
+
+ctr = Controller(gen_params)
+ctr.train()
